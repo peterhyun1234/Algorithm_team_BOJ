4e534d435d105504ccfb86bc8fc8872178e83984
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-07-28 19:08:35 +0900

    add some probs

--- HB/20200728/2075.cpp
+++ HB/20200728/2075.cpp
@@ -3,33 +3,61 @@
 // 이렇게 푸는 거 아닌거 같은데... ㅋㅋㅋㅋ 메모리초과 날줄 알았는데
 // 원래 방법 찾아보자
 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+    
+//     int n;
+//     cin >> n;
+//     //vector<vector<int>> numbers(n, vector<int>(n));
+//     vector<int> numbers(n*n);
+
+//     //for (int i = 0; i < n; i++){
+//         for (int j = 0; j < n*n; j++) {
+//             cin >> numbers[j];
+//         }
+//     //}
+
+//     sort(numbers.begin(), numbers.end(), greater<>());
+
+//     cout << numbers[n-1] << "\n";
+
+//     return 0;
+// }
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
+int main() {
+    ios::sync_with_stdio(false);
     cin.tie(0);
-    cout.tie(0);
     
-    int n;
-    cin >> n;
-    //vector<vector<int>> numbers(n, vector<int>(n));
-    vector<int> numbers(n*n);
-
-    //for (int i = 0; i < n; i++){
-        for (int j = 0; j < n*n; j++) {
-            cin >> numbers[j];
+    priority_queue<int, vector<int>, greater<>> pq;
+    int num;
+    cin >> num;
+    
+    for (int i = 0; i < num * num; i++) {
+        int tmp;
+        cin >> tmp;
+        if (pq.size() < num) pq.push(tmp);
+        else {
+            pq.push(tmp);
+            pq.pop();
         }
-    //}
-
-    sort(numbers.begin(), numbers.end(), greater<>());
-
-    cout << numbers[n-1] << "\n";
-
+    }
+    
+    cout << pq.top() << '\n';
+    
     return 0;
 }
