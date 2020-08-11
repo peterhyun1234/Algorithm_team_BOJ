513882870e8729c22afce02af2499e93a1dec07c
joon2974 <vnfmsdyd4@naver.com>
2020-08-11 12:03:34 +0900

    7week

--- YJ/20200811/17471.cpp
+++ YJ/20200811/17471.cpp
@@ -7,27 +7,180 @@
 //
 
 #include <iostream>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <cmath>
 
 using namespace std;
+
+int N, a, b, min_val = INT_MAX;
+int people_of_city[11], graph[11][11] = { 0 }, cpy_graph[11][11], chk[11] = { 0 };
+
+void reset_graph() {
+    for (int i = 1; i <= N; i ++) {
+        for (int j = 1; j <= N; j ++) {
+            graph[i][j] = cpy_graph[i][j];
+        }
+    }
+}
+
+struct Edge {
+    int node, dist;
+    Edge(int a, int c) {
+        node = a;
+        dist = c;
+    }
+};
+
+struct cmp {
+    bool operator() (Edge a, Edge b) {
+        if (a.dist > b.dist) return true;
+        else return false;
+    }
+};
+
+bool dijkstra(vector<int> chkArr) {
+    vector<int> dist(N + 1, INT_MAX);
+    priority_queue<Edge, vector<Edge>, cmp> Q;
+    int startNode = chkArr.back();
+    
+    dist[startNode] = 0;
+    Q.push(Edge(startNode, 0));
+    
+    while(!Q.empty()) {
+        int now_node = Q.top().node;
+        int now_dist = Q.top().dist;
+        Q.pop();
+        
+        if (dist[now_node] < now_dist) continue;
+        
+        for (int i = 1; i <= N; i ++) {
+            if (graph[now_node][i] == 1) {
+                int next_node = i;
+                int next_dist = 1 + now_dist;
+                
+                if (dist[next_node] > next_dist) {
+                    dist[next_node] = next_dist;
+                    Q.push(Edge(next_node, next_dist));
+                }
+            }
+        }
+    }
+    
+    for (int i = 1; i <= chkArr.size(); i ++) {
+        if (dist[i] == INT_MAX) {
+            return false;
+        }
+    } // 하나라도 갈 수 없을 경우
+    return true; // 연결 O
+}
+
+void DFS(int Level) {
+    if (Level == N + 1) {
+        // chk가 모두 0, 모두 1인경우 제외
+        bool flag = true;
+        int tmp = chk[1];
+        int sum1 = 0, sum2 = 0;
+        vector<int> set1;
+        vector<int> set2;
+        
+        for (int i = 2; i <= N; i ++) {
+            if (chk[i] != tmp) flag = false ;
+        }
+        
+        if (flag) return;
+        
+        
+        for (int i = 1; i <= N; i ++) {
+            if (chk[i] == 1) cout << i << ' ';
+        }
+        cout << '\n';
+        
+        
+        for (int i = 1; i <= N; i ++) { // 다른 집합 간선 끊기
+            if (chk[i] == 1) {
+                set1.push_back(i);
+                sum1 += people_of_city[i];
+                for (int j = 1; j <= N; j ++) {
+                    if (chk[j] == 0) graph[i][j] = 0;
+                }
+            } else {
+                set2.push_back(i);
+                sum2 += people_of_city[i];
+                for (int j = 1; j <= N; j ++) {
+                    if (chk[j] == 1) graph[i][j] = 0;
+                }
+            }
+        }
+        
+        cout << sum1 << ' '<< sum2 << '\n';
+        cout << dijkstra(set1) << ' ' << dijkstra(set2) << '\n';
+        
+        if (set1.size() == 1) {
+            // set2만 검사
+            if (dijkstra(set2) == false) {
+                reset_graph();
+                return;
+            }
+        } else if (set2.size() == 1) {
+            // set1만 검사
+            if (dijkstra(set1) == false) {
+                reset_graph();
+                return;
+            }
+        } else {
+            // 둘다 검사
+            if (dijkstra(set1) == false || dijkstra(set2) == false) {
+                reset_graph();
+                return;
+            }
+        }
+        
+        if (abs(sum1 - sum2) < min_val) min_val = abs(sum1 - sum2);
+        reset_graph();
+        
+    } else {
+        chk[Level] = 1;
+        DFS(Level + 1);
+        chk[Level] = 0;
+        DFS(Level + 1);
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     
-    int N, a, b, people_of_city[11], graph[11][11] = { 0 };
-    
     cin >> N;
+    
     for (int i = 1; i <= N; i ++) {
         cin >> people_of_city[i];
     }
+    
     for (int i = 1; i <= N; i ++) {
         cin >> a;
         for (int j = 0; j < a; j ++) {
             cin >> b;
             graph[i][b] = 1;
+            cpy_graph[i][b] = 1;
         }
     }
+    
+    
+    for (int i = 1; i <= N; i ++) {
+        for (int j = 1; j <= N; j ++) {
+            cout << graph[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    
+     
+    DFS(1);
+    
+    if (min_val == INT_MAX) cout << -1 << '\n';
+    else cout << min_val << '\n';
     
     return 0;
 }
---
+++ YJ/20200811/2234.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int board[50][50];
+int d[50][50];
+int rooms[50 * 50];
+
+// 서 북 동 남
+const int dy[4] = { 0,-1,0,1 };
+const int dx[4] = { -1,0,1,0};
+int solve(int y, int x, int roomNum)
+{
+    queue<pair<int, int>> q;
+    q.push({ y,x });
+    d[y][x] = roomNum;
+    int cnt = 0;
+    while (!q.empty())
+    {
+        y = q.front().first;
+        x = q.front().second;
+        // 방의 넓이 카운트
+        cnt++;
+        q.pop();
+        for (int i = 0; i < 4; ++i)
+        {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            // 범위 체크
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            // 방문 체크
+            if (d[ny][nx] != 0) continue;
+            // 벽 체크
+            if (board[y][x] & (1 << i)) continue;
+            
+            // ny,nx는 몇번방인지
+            d[ny][nx] = roomNum;
+            q.push({ ny,nx });
+        }
+    }
+    return cnt;
+}
+int main()
+{
+    cin >> n >> m;
+    for(int i=0; i<m; ++i)
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> board[i][j];
+        }
+
+    int num = 0;
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j) {
+            if (d[i][j] == 0) {
+                num++;
+                // num번방의 넓이는 = solve(i,j,num);
+                rooms[num] = solve(i, j, num);
+            }
+        }
+    // 개수 출력
+    cout << num << "\n";
+
+    int ret = 0;
+    // 가장 큰 넓이 구한다
+    for (int i = 1; i <= num; ++i)
+    {
+        ret = max(ret, rooms[i]);
+    }
+    cout << ret << "\n";
+
+    // d 에는 각 방의 번호가 적혀있고
+    // 합칠수있는 방이라면 합친후 최대값 계산
+    ret = 0;
+    for(int i=0; i<m; ++i)
+        for (int j = 0; j < n; ++j)
+        {
+            int y = i;
+            int x = j;
+            for (int k = 0; k < 4; ++k)
+            {
+                int ny = y + dy[k];
+                int nx = x + dx[k];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+                if (d[ny][nx] == d[y][x]) continue;
+                // 벽일때 뿌수고 최대값 구해야한다.
+                if (board[y][x] & (1 << k)) {
+                    ret = max(ret, rooms[d[ny][nx]] + rooms[d[y][x]]);
+                }
+            }
+        }
+    cout << ret << "\n";
+    return 0;
+}
