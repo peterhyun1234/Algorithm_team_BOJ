aaaeb5d0dbbae8a65ab471de6a9ed85b04574781
joon2974 <vnfmsdyd4@naver.com>
2020-08-13 01:59:08 +0900

    modify bellman-ford

--- YJ/20200814/11657.cpp
+++ YJ/20200814/11657.cpp
@@ -8,65 +8,6 @@
 
 #include <iostream>
 #include <vector>
-#define INF 99999999
-
-using namespace std;
-
-int N, M;
-vector<pair<int, int>> v[502];
-long long dist[501] = { INF, };
-
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int a, b, c;
-    bool cycle = false;
-    
-    cin >> N >> M;
-    
-    while (M--) {
-        cin >> a >> b >> c;
-        v[a].push_back(make_pair(b, c));
-    }
-
-    for (int i = 1; i <= N; i++) {
-        dist[i] = INF;
-    }
-    
-    dist[1] = 0;
-
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            for (int k = 0; k < v[j].size(); k++) {
-                int cur = v[j][k].first;
-                int cost = v[j][k].second;
-                if (dist[j] != INF && j != cur && dist[cur] > cost + dist[j]) {
-                    dist[cur] = cost + dist[j]; //최소값으로 갱신
-                    if (i == N) cycle = true;
-                }
-            }
-        }
-    }
-
-    if (cycle) cout << -1 << endl;
-    
-    else
-    {
-        for (int i = 2; i <= N; i++)
-        {
-            if (dist[i] == INF) cout << -1 << endl;
-            else cout << dist[i] << endl;
-        }
-
-    }
-    return 0;
-}
-
-/*
-#include <iostream>
-#include <vector>
 #include <climits>
 
 using namespace std;
@@ -90,7 +31,7 @@ int main() {
     
     cin >> N >> M;
     
-    vector<int> dist(N + 1, INT_MAX);
+    vector<long long> dist(N + 1, INT_MAX);
 
     for (int i = 0; i < M; i ++) {
         cin >> a >> b >> c;
@@ -128,4 +69,4 @@ int main() {
     
     return 0;
 }
-*/
+
